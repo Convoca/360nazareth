3d3b5b2ac74026801380947b65f6eec8f6358ff4
Elvis Rivera <ecriverac@gmail.com>
2020-07-07 19:42:34 -0500

    version 0.0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,47 @@
 
-var onLoadComplete = function(){
+var onLoadComplete = function () {
+    //alert("v 0.0.2");
     pannellum.viewer('panorama', {
         "type": "equirectangular",
         "panorama": "assets/360/portada.jpg",
+        "preview": "assets/images/preview.jpg",
         "compass": false,
-        "northOffset": 247.5,
-        "autoRotate":-3,
-        "autoLoad":true
+        //"northOffset": 90.5,
+        "autoLoad": true,
+        "ignoreGPanoXMP": true,
+        "showZoomCtrl": false
+    });
+
+    $(".menu").click(function () {
+        $(".menu").fadeOut("fast");
+        $(".menu-over").fadeIn();
+    });
+
+    $(".menu-over .close").click(function () {
+        $(".menu-over").fadeOut("fast");
+        $(".menu").fadeIn();
+    });
+
+    $(".sitios").click(function () {
+        $(".sitios").fadeOut("fast");
+        $(".sitios-over").fadeIn();
+    });
+
+    $(".sitios-over .close").click(function () {
+        $(".sitios-over").fadeOut("fast");
+        $(".sitios").fadeIn();
+    });
+    
+}
+
+// function show pannellum
+function showPannellum(image) {
+    pannellum.viewer('panorama', {
+        "type": "equirectangular",
+        "panorama": "assets/360/" + image + ".jpg",
+        "compass": false,
+        "autoLoad": true,
+        "ignoreGPanoXMP": true,
+        "showZoomCtrl": false
     });
 }
---
+++ assets/zoom_mobile/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1527200690,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/maps-sheet0.png",
+		"images/zoomin-sheet0.png",
+		"images/zoomout-sheet0.png",
+		"images/dato-sheet0.png",
+		"images/dato-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
