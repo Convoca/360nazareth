7e00c55d563a881f2d6141057d820278812db252
Elvis Rivera <ecriverac@gmail.com>
2020-07-08 00:42:22 -0500

    version 0.0.7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 
+var myPlayer;
+var myAudio;
 var onLoadComplete = function () {
     //alert("v 0.0.2");
     pannellum.viewer('panorama', {
@@ -9,8 +11,8 @@ var onLoadComplete = function () {
         //"northOffset": 90.5,
         "autoLoad": true,
         "ignoreGPanoXMP": true,
-        "showZoomCtrl": false
-    });
+        "showZoomCtrl": false,
+    }).startAutoRotate();
 
     $(".menu").click(function () {
         $(".menu").fadeOut("fast");
@@ -60,16 +62,59 @@ var onLoadComplete = function () {
         $(".galleria-container").remove();
         $("body").css("overflow","auto");
     });
+
+    
+    myAudio = new Audio();
+    myAudio.src = "assets/audio/fondo.mp3"
+    myAudio.loop = true;
+
+    $("body").mouseover(function(){
+        if(myAudio.paused) myAudio.play();
+    });
+
+    $(".audio").click(function(){
+        if(myAudio.volume==0){
+            myAudio.volume=1;
+        }else{
+            myAudio.volume=0;
+        }
+       
+    });
+
 }
 
 // function show pannellum
 function showPannellum(image) {
-    pannellum.viewer('panorama', {
-        "type": "equirectangular",
-        "panorama": "assets/360/" + image + ".jpg",
-        "compass": false,
-        "autoLoad": true,
-        "ignoreGPanoXMP": true,
-        "showZoomCtrl": false
-    });
+    $(".video-js").hide();
+
+    if(image=="rio") $('.leyenda').html("Las aguas del Chiriaco se contaminaron de petróleo el 2016. Hoy están limpias.");
+    if(image=="quebrada") $('.leyenda').html("En esta zona de la quebrada Inayo se quebró el ducto de Petroperú.");
+    if(image=="gente") $('.leyenda').html("Algunos habitantes de Nazareth recogieron el crudo con sus propias manos.");
+    
+    
+
+    if(image!="colegio"){
+        pannellum.viewer('panorama', {
+            "type": "equirectangular",
+            "panorama": "assets/360/" + image + ".jpg",
+            "compass": false,
+            "autoLoad": true,
+            "ignoreGPanoXMP": true,
+            "showZoomCtrl": false,
+            "yaw":-110,
+        }).startAutoRotate();
+        myPlayer.pause();
+    }else{
+        myPlayer=videojs('panoramavideo', {
+            plugins: {
+                controls: false,
+                autoplay: true,
+                pannellum: {}
+            }
+        });
+        myPlayer.play();
+        $('.leyenda').html("La escuela de la comunidad es fuente de esperanza. Sólo es de nivel primario.");
+        $(".video-js").show();
+    }
+
 }
